1d7739488b668cbd6d80200a24c5242d7e65b5dd
VICTORIA SANTOS AMARAL DE OLIVEIRA <victoria.saoliveira@senacsp.edu.br>
2026-08-25 21:50:15 -0300

    Adicionando exercicio 1 da aula 3

---
+++ aula3/script.js
@@ -0,0 +1,8 @@
+const butao = document.querySelector('#mostrarbtn');
+const noticias = document.querySelector('#noticias');
+
+butao.addEventListener('click',()=>{
+    document.noticias.classList.toggle("mostrarnoticias");
+
+
+})
